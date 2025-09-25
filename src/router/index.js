0af2bd55ebc76e644a8febfcb7fe4137e2e836a7
haruoti <hazrul.izhar90@gmail.com>
2025-09-25 11:31:07 +0800

    feat: add edit redemption functionality - Implemented a new editRedemption component and route, allowing users to modify shipping details for redemptions, enhancing user experience and data management.

--- src/router/index.js
+++ src/router/index.js
@@ -264,6 +264,11 @@ const router = createRouter({
                     name: 'Detail-Redemption',
                     component: () => import('@/views/marketing/detailRedemption.vue')
                 },
+                {
+                    path: '/marketing/editRedemption',
+                    name: 'Edit-Redemption',
+                    component: () => import('@/views/marketing/editRedemption.vue')
+                },
             ]
         },
         {
