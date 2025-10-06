1d7d7f0c9b8f441a9d5cb8d7166bb9151636693b
hdin1464@gmail.com <82224707+fixlynax@users.noreply.github.com>
2025-10-06 14:27:30 +0800

    `feat: updated technical detail page with new features and layout changes`

--- src/views/technical/createScrap.vue
+++ src/views/technical/createScrap.vue
@@ -3,32 +3,42 @@
         <div class="flex flex-col md:flex-row gap-8">
             <div class="card flex flex-col gap-6 w-full">
                 <!-- Header -->
-                <div class="text-2xl font-bold text-gray-800 border-b pb-2">Create CTC</div>
+                <div class="text-2xl font-bold text-gray-800 border-b pb-2">Create Scrap</div>
 
                 <!-- Dealer Details -->
                 <div>
-                    <div class="text-xl font-bold text-gray-800 border-b pb-2 mb-4">🏢 Waranty Details</div>
+                    <div class="text-xl font-bold text-gray-800 border-b pb-2 mb-4">🏢 Scrap Details</div>
                     <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
                         <div>
-                            <label class="block font-bold text-gray-700">Entry ID </label>
-                            <InputText class="w-full" disable />
+                            <label class="block font-bold text-gray-700">Damage Code</label>
+                            <InputText class="w-full" />
                         </div>
 
                         <div>
-                            <label class="block font-bold text-gray-700">Collect Date</label>
+                            <label class="block font-bold text-gray-700">Description</label>
+                            <InputText class="w-full" />
+                        </div>
+
+                        <div>
+                            <label class="block font-bold text-gray-700">Claim %</label>
                             <Calendar class="w-full" placeholder="Select Date " showIcon icon="pi pi-calendar" />
                         </div>
 
                         <div>
-                            <label class="block font-bold text-gray-700">Collect Time</label>
+                            <label class="block font-bold text-gray-700">Usable %</label>
                             <Calendar timeOnly hourFormat="12" class="w-full" placeholder="Select time " showIcon icon="pi pi-clock" />
+                        </div>
+
+                        <div>
+                            <label class="block font-bold text-gray-700">Worn %</label>
+                            <InputText class="w-full" />
                         </div>
                     </div>
 
                     <div class="flex justify-start mt-8">
                         <div class="w-40">
                             <RouterLink to="/technical/detailWarantyClaim">
-                                <Button label="Sumbit Order" class="w-full p-button-success" />
+                                <Button label="Submit" class="w-full p-button-success" />
                             </RouterLink>
                         </div>
                     </div>
