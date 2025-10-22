ff7e5bc215418410ba39b14a0450381116e47139
DESKTOP-95QAV4H\lenovo <aniskasmuri1166@gmail.com>
2025-10-22 12:48:30 +0800

    Refactor detailMessaging route to use a simplified path and update component import

--- src/router/index.js
+++ src/router/index.js
@@ -223,10 +223,9 @@ const router = createRouter({
                     component: () => import('@/views/om/listMessaging.vue')
                 },
                 {
-                    path: '/om/detailMessaging/:messageId',
-                    name: 'detailMessaging',
-                    component: () => import('@/views/detailMessaging.vue'),
-                    props: true
+                    path: '/om/detailMessaging',
+                    name: 'Detail-Messaging',
+                    component: () => import('@/views/om/detailMessaging.vue')
                 },
                 {
                     path: '/om/createMessaging',
