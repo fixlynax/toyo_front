ac12123e559f668d31423fb19d7ba82688c89151
Den\Dennn 😄 <danialaqil222@gmail.com>
2025-11-15 12:32:48 +0800

    Technical - Warranty Claim Details

--- src/views/technical/detailWarantyClaim.vue
+++ src/views/technical/detailWarantyClaim.vue
@@ -1075,7 +1075,7 @@ const loadScrapImages = async () => {
 // Initialize workflow states
 const initializeWorkflowStates = () => {
     // Set claim status
-    if (warantyDetail.value.status === 4) {
+    if (warantyDetail.value.status === 5) {
         claimFinalStatus.value = 'approved';
     } else if (warantyDetail.value.status === 6) {
         claimFinalStatus.value = 'rejected';
