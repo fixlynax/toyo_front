b7cc8c00fe24300c99a29b71916a5e0f92314509
Den\Dennn 😄 <danialaqil222@gmail.com>
2025-11-27 20:45:54 +0800

    SCM - Date format,  use ClaimID instead of Id, remove export toggle for completed status

--- src/views/scm/listReturnOrder.vue
+++ src/views/scm/listReturnOrder.vue
@@ -428,10 +428,6 @@ function formatDate(dateString) {
     year: 'numeric',
     month: '2-digit',
     day: '2-digit',
-    hour: '2-digit',
-    minute: '2-digit',
-    second: '2-digit',
-    hour12: true
   });
 }
 
