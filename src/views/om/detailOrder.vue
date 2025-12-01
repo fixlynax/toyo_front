2b99fb99448276bc0f9501b570b37e00be4fea80
hdin1464@gmail.com <82224707+fixlynax@users.noreply.github.com>
2025-12-01 21:35:57 +0800

    `Update detailOrder.vue to use deliveryInfo.storagelocation instead of orderData.storagelocation`

--- src/views/om/detailOrder.vue
+++ src/views/om/detailOrder.vue
@@ -133,7 +133,7 @@
                                 </tr>
                                 <tr class="border-b">
                                     <td class="px-4 py-2 font-medium">Storage Location</td>
-                                    <td class="px-4 py-2 text-right font-medium">{{ orderData.storagelocation || '-' }}</td>
+                                    <td class="px-4 py-2 text-right font-medium">{{ deliveryInfo.storagelocation || '-' }}</td>
                                 </tr>
                                 <tr class="border-b">
                                     <td class="px-4 py-2 font-medium">Planned Date</td>
