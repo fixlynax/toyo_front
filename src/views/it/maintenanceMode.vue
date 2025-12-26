1e5e40ab419750da3114f8f2de214001526cf36b
DESKTOP-95QAV4H\lenovo <aniskasmuri1166@gmail.com>
2025-12-26 10:47:46 +0800

    Add sortable functionality to table columns in multiple views

--- src/views/it/maintenanceMode.vue
+++ src/views/it/maintenanceMode.vue
@@ -9,12 +9,12 @@
                 </div>
 
                 <DataTable :value="versionList" :loading="loading" class="rounded-table">
-                    <Column field="type" header="Type">
+                    <Column field="type" header="Type" sortable>
                         <template #body="{ data }">
                             <span class="font-medium">{{ getTypeLabel(data.type) }}</span>
                         </template>
                     </Column>
-                    <Column field="currentVer" header="Current Version"></Column>
+                    <Column field="currentVer" header="Current Version" sortable></Column>
                     <Column field="newVer" header="New Version"></Column>
                     <Column field="link" header="Link">
                         <template #body="{ data }">
@@ -24,17 +24,17 @@
                             <span v-else class="text-gray-400">-</span>
                         </template>
                     </Column>
-                    <Column field="isForce" header="Force Update">
+                    <Column field="isForce" header="Force Update" sortable>
                         <template #body="{ data }">
                             <Tag :value="data.isForce ? 'Yes' : 'No'" :severity="data.isForce ? 'danger' : 'success'" />
                         </template>
                     </Column>
-                    <Column field="isMaintainence" header="Maintenance">
+                    <Column field="isMaintainence" header="Maintenance" sortable>
                         <template #body="{ data }">
                             <Tag :value="data.isMaintainence ? 'Yes' : 'No'" :severity="data.isMaintainence ? 'warning' : 'info'" />
                         </template>
                     </Column>
-                    <Column header="Actions" style="width: 120px">
+                    <Column header="Actions" style="width: 120px" >
                         <template #body="{ data }">
                             <div class="flex gap-2">
                                 <Button icon="pi pi-eye" class="p-button-rounded p-button-text p-button-sm" @click="viewMaintenanceMsg(data)" v-tooltip="'View Maintenance Message'" />
