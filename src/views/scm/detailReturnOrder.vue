88d329e9fe5875b4f1e0df0907075a6c025160f2
Den\Dennn 😄 <danialaqil222@gmail.com>
2026-01-16 09:36:52 +0800

    ui fix

--- src/views/scm/detailReturnOrder.vue
+++ src/views/scm/detailReturnOrder.vue
@@ -156,7 +156,7 @@
                                     <td class="px-4 py-2 font-medium">Inv No</td>
                                     <td class="px-4 py-2 text-right">{{ returnList.order_data.inv_no || '-' }}</td>
                                 </tr>
-                                <tr>
+                                <tr class="border-b">
                                     <td class="px-4 py-2 font-medium">SAP Created</td>
                                     <td class="px-4 py-2 text-right">{{ formatDateTime(returnList.sap_timestamp) }}</td>
                                 </tr>
